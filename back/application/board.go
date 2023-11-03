e5eb33e470829c900e6a84ee49b915bcea4878df
yuorei <108039575+yuorei@users.noreply.github.com>
2023-11-03 16:36:59 +0900

    コメント

--- back/application/board.go
+++ back/application/board.go
@@ -176,3 +176,116 @@ func (s *boardService) GetBoardList(ctx context.Context, _ *emptypb.Empty) (*grp
 		Boards: boardList,
 	}, nil
 }
+
+func (s *boardService) GetCommentById(ctx context.Context, request *grpc_board.ID) (*grpc_board.Comment, error) {
+	board, err := s.db.GetCommentById(request.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	createAt := timestamppb.New(board.CreatedAt)
+	updateAt := timestamppb.New(board.UpdatedAt)
+
+	return &grpc_board.Comment{
+		Id:          board.ID,
+		PostId:      board.PostID,
+		CommenterId: board.CommenterID,
+		Content:     board.Content,
+		CreatedAt:   createAt,
+		UpdatedAt:   updateAt,
+	}, nil
+}
+
+func (s *boardService) CreateComment(ctx context.Context, request *grpc_board.Comment) (*grpc_board.Comment, error) {
+	now := time.Now()
+	board := model.Comment{
+		ID:          uuid.New().String(),
+		PostID:      request.PostId,
+		CommenterID: request.CommenterId,
+		Content:     request.Content,
+		CreatedAt:   now,
+		UpdatedAt:   now,
+	}
+
+	err := s.db.CreateComment(&board)
+	if err != nil {
+		return nil, err
+	}
+
+	createAt := timestamppb.New(time.Now())
+	updateAt := createAt
+
+	return &grpc_board.Comment{
+		Id:          board.ID,
+		PostId:      board.PostID,
+		CommenterId: board.CommenterID,
+		Content:     board.Content,
+		CreatedAt:   createAt,
+		UpdatedAt:   updateAt,
+	}, nil
+}
+
+func (s *boardService) UpdateComment(ctx context.Context, request *grpc_board.Comment) (*grpc_board.Comment, error) {
+	now := time.Now()
+	board := &model.Comment{
+		ID:          uuid.New().String(),
+		PostID:      request.PostId,
+		CommenterID: request.CommenterId,
+		Content:     request.Content,
+		CreatedAt:   now,
+		UpdatedAt:   now,
+	}
+
+	err := s.db.UpdateComment(board.ID, board)
+	if err != nil {
+		return nil, err
+	}
+
+	createAt := request.CreatedAt
+	updateAt := timestamppb.New(now)
+
+	return &grpc_board.Comment{
+		Id:          board.ID,
+		PostId:      board.PostID,
+		CommenterId: board.CommenterID,
+		Content:     board.Content,
+		CreatedAt:   createAt,
+		UpdatedAt:   updateAt,
+	}, nil
+}
+
+func (s *boardService) DeleteComment(ctx context.Context, request *grpc_board.ID) (*emptypb.Empty, error) {
+	err := s.db.DeleteComment(request.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *boardService) GetCommentList(ctx context.Context, _ *emptypb.Empty) (*grpc_board.CommentList, error) {
+	boards, err := s.db.GetCommentList()
+	if err != nil {
+		return nil, err
+	}
+
+	var boardList []*grpc_board.Comment
+
+	for _, board := range boards {
+		createAt := timestamppb.New(board.CreatedAt)
+		updateAt := timestamppb.New(board.UpdatedAt)
+
+		boardList = append(boardList, &grpc_board.Comment{
+			Id:          board.ID,
+			PostId:      board.PostID,
+			CommenterId: board.CommenterID,
+			Content:     board.Content,
+			CreatedAt:   createAt,
+			UpdatedAt:   updateAt,
+		})
+	}
+
+	return &grpc_board.CommentList{
+		Comments: boardList,
+	}, nil
+}
--- back/db/board.go
+++ back/db/board.go
@@ -38,3 +38,40 @@ func (d *DB) DeleteBoard(id string) error {
 	}
 	return nil
 }
+
+func (d *DB) GetCommentById(id string) (*model.Comment, error) {
+	var comment *model.Comment
+	if err := d.Conn.Where("id = ?", id).First(&comment).Error; err != nil {
+		return nil, err
+	}
+	return comment, nil
+}
+
+func (d *DB) GetCommentList() ([]*model.Comment, error) {
+	var comments []*model.Comment
+	if err := d.Conn.Find(&comments).Error; err != nil {
+		return nil, err
+	}
+	return comments, nil
+}
+
+func (d *DB) CreateComment(comment *model.Comment) error {
+	if err := d.Conn.Create(comment).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *DB) UpdateComment(id string, comment *model.Comment) error {
+	if err := d.Conn.Where("id = ?", id).Updates(comment).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *DB) DeleteComment(id string) error {
+	if err := d.Conn.Where("id = ?", id).Delete(&model.Comment{}).Error; err != nil {
+		return err
+	}
+	return nil
+}

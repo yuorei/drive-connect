564e29cab62304d9b73b4c66aa1600644edea091
yuorei <108039575+yuorei@users.noreply.github.com>
2023-11-03 16:09:37 +0900

    fix table

--- back/db/model/model.go
+++ back/db/model/model.go
@@ -15,28 +15,19 @@ type User struct {
 	UpdatedAt       time.Time
 }
 
-type DriverBoard struct {
-	ID          string `gorm:"primaryKey"`
-	DriverID    string
-	Title       string
-	Description string
-	Reward      string
-	StartTime   time.Time
-	EndTime     time.Time
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-}
-
-type PassengerBoard struct {
-	ID          string `gorm:"primaryKey"`
-	PassengerID string
-	Title       string
-	Description string
-	Reward      string
-	StartTime   time.Time
-	EndTime     time.Time
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+type Board struct {
+    ID                   string `gorm:"primaryKey"`
+    Type                 string
+    UserID               string
+    Description          string
+    DepartureLatitude    float64
+    DepartureLongitude   float64
+    DestinationLatitude  float64
+    DestinationLongitude float64
+    Reward               string
+    StartTime            time.Time
+    CreatedAt            time.Time
+    UpdatedAt            time.Time
 }
 
 type Comment struct {

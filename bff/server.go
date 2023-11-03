da35127ba199edaa8488936d5573724f6abf913b
yuorei <108039575+yuorei@users.noreply.github.com>
2023-11-04 04:43:00 +0900

    cors

--- bff/server.go
+++ bff/server.go
@@ -26,9 +26,30 @@ func main() {
 	c.Directives.Auth = directive.Auth
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(c))
 
+	// CORSのハンドラを設定
+	corsHandler := allowCORS(srv)
+
 	http.Handle("/", playground.Handler("GraphQL playground", "/graphql"))
-	http.Handle("/graphql", middleware.AuthMiddleware(srv))
+	http.Handle("/graphql", middleware.AuthMiddleware(corsHandler))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+// CORSを許可するハンドラ
+func allowCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// ヘッダーの設定
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
+
+		// オプションリクエストの場合は即座にレスポンスを返す
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		// 通常のリクエストの場合はハンドラを呼び出す
+		h.ServeHTTP(w, r)
+	})
 }
